b06faaec4ea715ee90db38cf64e5e3976842ae99
Vikash Singh <vikassingh1999999@gmail.com>
2025-11-14 04:45:30 +0530

    feat: initialized user routes

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 # The post file have basic CRUD function for posts
 # This application has a authMiddleware which authorize token for users signin and also for posts
 # In authMiddleware, it'll fetch request from Authorization header to authorize tokens and checks if token exist and start with Bearer, if exist it extract JWT token from the header and decode the token with secret key, if token is invalid or expired the try & catch block will throw an error or if token is decoded, it'll be store in userId
-# 
+# In user.ts file, we have signup route for users to register themselves
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,40 @@
-import router from ".";
+import { Router } from "express";
+import 	jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+const router = Router();
+
+router.post("/signup", async (req, res) => {
+	const existingUser = await prisma.user.findUnique({
+		where: {email: req.body.email}
+	})
+
+	if(existingUser) {
+		return res.status(400).json({
+			message: "User already exists"
+		});
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email: req.body.email,
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			password: req.body.password
+		},
+	});
+
+	const userId = user.id;
+
+	const token = jwt.sign({userId}, JWT_SECRET, {expiresIn: "1h"});
+
+	res.status(201).json({
+		message: "user created",
+		token
+	});
+} )
 
 
 export default router;
