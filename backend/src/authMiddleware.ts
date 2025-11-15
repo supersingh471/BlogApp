8833f56e69d5b6ecef38c37281d61f2f9a613b4e
Vikash Singh <vikassingh1999999@gmail.com>
2025-11-16 05:02:35 +0530

    feat: initialize authMiddleware and posts router

---
+++ backend/src/authMiddleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "./config";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface JwtUserPayload extends JwtPayload {
+  userId: number;
+}
+
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+	const authHeader = req.headers.authorization;
+
+	if(!authHeader || !authHeader.startsWith('Bearer')) {
+		return res.status(400).json({
+			message: "Error while logging"
+		})
+	}
+	const token = authHeader.split("")[1];
+
+	try{
+		const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+		req.userId = decoded.userId;
+		next();
+	}catch(error) {
+		console.error("Token verification failed");
+	}
+
+}
+
+export default authMiddleware;
--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,7 +1,26 @@
-import router from "./user";
+import { Router } from "express";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import authMiddleware from "../authMiddleware";
 
+const prisma = new PrismaClient();
+const router = Router();
 
+router.post("/posts", authMiddleware, async (req: Request, res: Response) => {
+	const {title, content} = req.body
 
+	const blog = await prisma.posts.create({
+		data: {
+				title,
+				content,
+				authorId: req.userId!
+		},
+	}) 
 
+	return res.status(200).json({
+		message: "Blog posted",
+		blog
+	})
+})
 
 export default router;
