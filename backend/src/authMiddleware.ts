e2492b342e03ac2d0ef26dfcd8b01038f7be66e9
Vikash Singh <vikassingh1999999@gmail.com>
2025-11-17 03:18:06 +0530

    feat: completed posts routes

--- backend/src/authMiddleware.ts
+++ backend/src/authMiddleware.ts
@@ -9,19 +9,21 @@ interface JwtUserPayload extends JwtPayload {
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 	const authHeader = req.headers.authorization;
 
-	if(!authHeader || !authHeader.startsWith('Bearer')) {
+	if(!authHeader || !authHeader.startsWith("Bearer ")) {
 		return res.status(400).json({
 			message: "Error while logging"
 		})
 	}
-	const token = authHeader.split("")[1];
+	const token = authHeader.split(" ")[1];
 
 	try{
 		const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
 		req.userId = decoded.userId;
 		next();
 	}catch(error) {
-		console.error("Token verification failed");
+		return res.status(403).json({
+        message: "Invalid or expired token"
+    });
 	}
 
 }
--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -21,6 +21,100 @@ router.post("/posts", authMiddleware, async (req: Request, res: Response) => {
 		message: "Blog posted",
 		blog
 	})
-})
+});
+
+router.get("/posts", authMiddleware, async (req, res) => {
+	const Id = Number(req.query.id);
+
+	try{
+		if(Id) {
+			const postItem = await prisma.posts.findFirst({where: {id: Id, authorId: req.userId}});
+			if(!postItem) {
+				return res.status(400).json({
+					message: "Post not found"
+				});
+			}
+
+			return res.status(200).json({
+				post: postItem
+			})
+		}
+	}catch(e) {
+		return res.status(400).json({
+			message: "Invalid id"
+		})
+	}
+});
+
+router.put("/posts", authMiddleware, async (req, res) => {
+	const { title, content } = req.body;
+	const Id = Number(req.query.id);
+
+	try{
+		const updatePost = await prisma.posts.updateMany({
+			where: {
+				id: Id,
+				authorId: req.userId
+			},
+			data: {
+				title,
+				content
+			}
+		})
+
+		if(!updatePost) {
+				return res.status(400).json({
+					message: "Blog not found or unauthorized"
+				})
+			}
+		
+		return res.status(200).json({
+			message: "Blog updated",
+			updatePost
+		})
+	}catch(e) {
+		res.status(500).json({
+			message: "Internal server error while updating"
+		})
+	}
+});
+
+router.delete("/posts/:id", authMiddleware, async (req, res) => {
+	const Id = Number(req.params.id);
+
+	if(!Id) {
+		return res.status(400).json({
+			message: "Invalid post ID"
+		})
+	}
+
+	try{
+		if(Id) {
+			const deletedPost = await prisma.posts.deleteMany({
+				where: {
+					id: Id,
+					authorId: req.userId
+				},
+			})
+
+			if(deletedPost.count === 0) {
+				return res.status(404).json({
+					message: "No post found or unauthorized"
+				})
+			}
+
+			return res.status(200).json({
+			message: "Post deleted successfully",
+			deletedPost
+		})
+		}
+
+	}catch(e) {
+			console.error(e)
+			return res.status(500).json({
+				message: "Internal server error while deleting"
+			})
+	}
+});
 
 export default router;
