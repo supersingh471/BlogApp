eb9c0cc5e1a2bc18fc83704f5bded9ff7c2f2bf2
Vikash Singh <vikassingh1999999@gmail.com>
2025-12-02 03:43:54 +0530

    working on dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
 	return (
 		<>
 		<BrowserRouter>
 		<Routes>
-			<Route></Route>
+			<Route path="/dashboard" element={<Dashboard/>}/>
 		</Routes>
 		</BrowserRouter>
 		</>
