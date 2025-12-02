54e582f6d177b54c2d6c8c130505075843aacc5a
Vikash Singh <vikassingh1999999@gmail.com>
2025-12-03 04:01:26 +0530

    working on frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+export function Heading({title} : {title: string}) {
+
+	return (
+		<div className="text-4xl text-white font-sans font-semibold">{title}</div>
+	)
+
+}
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,15 +1,19 @@
-
+import { Heading } from "../components/Header";
 
 export default function Dashboard() {
-
 	return (
-		<div className="min-h-screen w-full 
-		bg-[#0d0f13]
-		bg-[radial-gradient(circle_at_30%_20%,rgba(255,255,255,0.06)_0%,rgba(255,255,255,0.02)_25%,rgba(0,0,0,0)_60%)]
-		bg-[radial-gradient(circle_at_80%_80%,rgba(255,255,255,0.05)_0%,rgba(255,255,255,0.015)_30%,rgba(0,0,0,0)_70%)]
-		bg-[linear-gradient(180deg,#0d0f13_0%,#090a0f_50%,#050507_100%)]
-		bg-blend-overlay">
-		<div></div>
-	</div>
-	)
-}
+	  <div
+		className="
+		  min-h-screen w-full
+		  bg-[#0d0f13]
+		  bg-blend-overlay
+		  bg-[linear-gradient(180deg,#0d0f13_0%,#090a0f_50%,#050507_100%),
+			  radial-gradient(circle_at_30%_20%,rgba(255,255,255,0.06)_0%,rgba(255,255,255,0.02)_25%,rgba(0,0,0,0)_60%),
+			  radial-gradient(circle_at_80%_80%,rgba(255,255,255,0.05)_0%,rgba(255,255,255,0.015)_30%,rgba(0,0,0,0)_70%)]">
+		<div className="flex border shadow-amber-200">
+			<div><Heading title="Blog"/></div>
+		</div>
+	  </div>
+	);
+  }
+  
