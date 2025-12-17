b0f0a10f0c2d1f2ade2004aaeb8f432afec17dcc
Vikash Singh <vikassingh1999999@gmail.com>
2025-12-18 03:00:43 +0530

    created blog list on Dashbaord

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,13 +1,46 @@
-
 import { Heading } from "../components/Header";
+import { useState } from "react";
 
 export default function Dashboard() {
+
+	const [blogs,setBlogs] = useState([
+		{
+			id:1,
+			title: "Building a Blogging website",
+			content: "This application is created for blogging where users can post blog on any topic",
+			author: "Vikash Singh",
+			createdAt: "18 Dec 2025"
+		},
+		{
+			id:2,
+			title: "Building a Blogging website",
+			content: "This application is created for blogging where users can post blog on any topic",
+			author: "Vikash Singh",
+			createdAt: "18 Dec 2025"
+		},
+		{
+			id:3,
+			title: "Building a Blogging website",
+			content: "This application is created for blogging where users can post blog on any topic",
+			author: "Vikash Singh",
+			createdAt: "18 Dec 2025"
+		},
+		{
+			id:4,
+			title: "Building a Blogging website",
+			content: "This application is created for blogging where users can post blog on any topic",
+			author: "Vikash Singh",
+			createdAt: "18 Dec 2025"
+		},
+	]);
 	return (
-	  <div className="flex flex-col min-h-screen w-full bg-linear-to-b from-[#c8bced] to-white pt-8">
+	  <div className="min-h-screen w-full bg-linear-to-b from-[#c8bced] to-white pt-8">
 		<div className="mx-20 h-20 rounded-xl ring-1 ring-purple-500/50">
 			<div className="p-5 text-4xl"><Heading title="DevBlog"/></div>
 		</div>
-		<div className="mt-10 h-120 w-120 ml-20 mr-200 p-5 shadow-[0_0_20px_rgba(168,85,247,0.3)] rounded-xl">
+
+		<div className="flex mt-10 h-140 w-full">
+			<div className="mt-5 h-120 w-120 ml-20 p-5 shadow-[0_0_20px_rgba(168,85,247,0.3)] rounded-xl">
 			<div className="flex flex-col h-110 w-110 shadow-[0_0_20px_rgba(168,85,247,0.3)] rounded-md">
 				<div className="flex justify-between text-2xl p-4"><Heading title="Create New Post"/><svg className="w-[55px] h-[55px] fill-[#0a0a0a]" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
 				<path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"></path></svg>
@@ -20,7 +53,22 @@ export default function Dashboard() {
 				<path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"></path></svg>
 				</button>
 			</div>
+			</div>
+
+			<div className="grid grid-cols-2 gap-4 w-220 h-120 ml-10 mt-5 p-5 rounded-xl shadow-[0_0_20px_rgba(168,85,247,0.3)]">
+				{blogs.map((blog) => (
+					<div key={blog.id} className="h-full p-5 shadow-[0_0_20px_rgba(168,85,247,0.3)] rounded-md cursor-pointer transition-all duration-300 ease-out
+					hover:-translate-y-1
+					hover:scale-[1.02]
+					hover:shadow-2xl">
+						<h2 className="text-xl font-semibold">{blog.title}</h2>
+						<p className="mt-2 text-gray-700">{blog.content}</p>
+						<span className="mt-3 text-sm text-gray-500">{blog.author} . {blog.createdAt}</span>
+					</div>
+				))}	
 		</div>
+		</div>
+		
 	  </div>
 	);
   }
