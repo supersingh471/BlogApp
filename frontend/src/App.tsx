c925035fdcd9cf62606c805ab002daad8db65683
Vikash Singh <vikassingh1999999@gmail.com>
2025-12-26 23:18:36 +0530

    initiated login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Landing from "./pages/landing";
 import Signup from "./pages/signup";
+import Login from "./pages/login";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 			<Route path="/dashboard" element={<Dashboard/>}/>
 			<Route path="/landing" element={<Landing/>}/>
 			<Route path="/signup" element={<Signup/>}/>
+			<Route path="/login" element={<Login/>}/>
 		</Routes>
 		</BrowserRouter>
 		</>
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,10 @@
+import { Heading } from "../components/Header";
+
+export default function Login() {
+
+	return (
+		<div className="flex flex-col min-h-screen w-full bg-[linear-gradient(to_bottom,#F8B4F0,#C7F9F8)] backdrop-blur-md">
+			<div className="p-7 text-3xl font-semibold"><Heading title="DevBlog"/></div>
+		</div>
+	)
+}
