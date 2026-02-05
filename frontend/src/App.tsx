1a98d2129a5d1adad26400c780e95c812f08f58b
Vikash Singh <vikassingh1999999@gmail.com>
2026-02-06 04:22:39 +0530

    building new dashbaord

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Dashboard1 from "./pages/Dashboard1";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import Landing from "./pages/Landing";
@@ -12,6 +13,7 @@ function App() {
 		<Routes>
 			<Route path="/landing" element={<Landing/>}/>
 			<Route path="/dashboard" element={<Dashboard/>}/>
+			<Route path="/dashboard1" element={<Dashboard1/>}/>
 			<Route path="/signup" element={<Signup/>}/>
 			<Route path="/login" element={<Login/>}/>
 		</Routes>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,10 @@
+import { GiPenguin } from "react-icons/gi";
 
 export function Heading({title} : {title: string}) {
 
 	return (
-		<div>{title}</div>
+		<div className="flex p-7 text-3xl font-semibold gap-2">
+			<GiPenguin size={40}/>{title}</div>
 	)
 
 }
